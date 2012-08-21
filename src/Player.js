b6ed6e8c71538b362895e3268bbe8ffc7723cdb6
Steve Phillips <kingcoyote85@gmail.com>
2012-08-21 06:37:32 -0700

    slight cleanup. mostly just a push test

--- src/Player.js
+++ src/Player.js
@@ -16,7 +16,12 @@ function Player() {
     this.sprite = sprite;
 
     this.speed = 180;
-    this.movement = {up:false,down:false,left:false,right:false};
+    this.movement = { 
+        up    : false,
+        down  : false,
+        left  : false,
+        right : false
+    };
     this.scheduleUpdate();
 }
 
@@ -30,18 +35,19 @@ Player.inherit(cocos.nodes.Node, {
             pos.x -= this.speed * dt
         }
 
-        if(this.movement.right && geo.rectGetMaxX(box) <= win.width) {
+        if (this.movement.right && geo.rectGetMaxX(box) <= win.width) {
             pos.x += this.speed * dt
         }
 
-        if(this.movement.up && geo.rectGetMaxY(box) <= win.height) {
+        if (this.movement.up && geo.rectGetMaxY(box) <= win.height) {
             pos.y += this.speed * dt
         }
 
-        if(this.movement.down && geo.rectGetMinY(box) >= 0) {
+        if (this.movement.down && geo.rectGetMinY(box) >= 0) {
             pos.y -= this.speed * dt
         }
-
+        pos.x = Math.floor(pos.x);
+        pos.y = Math.floor(pos.y);
         this.position = pos
     }
 });
