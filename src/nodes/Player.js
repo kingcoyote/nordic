478e803713adff36b3b7d52c73e2321d1bebd35e
Steve Phillips <kingcoyote85@gmail.com>
2012-08-23 14:00:51 -0700

    work on the walking animation. it's close, but the mirroring isn't working right and he doesn't go to the right standing mode after stopping

--- src/nodes/Player.js
+++ src/nodes/Player.js
@@ -5,54 +5,14 @@ var cocos = require('cocos2d')
 function Player() {
     Player.superclass.constructor.call(this)
     
-    var frameSize = new geo.Size(30,30)
-
-    // begin aniatmion
-
-    var texture = new cocos.Texture2D({file: "/resources/player.png"});
-
-    var frame0 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 0, 0, frameSize.width, frameSize.height)})
-      , frame1 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 1, 0, frameSize.width, frameSize.height)})
-      , frame2 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 2, 0, frameSize.width, frameSize.height)})
-      , frame3 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 3, 0, frameSize.width, frameSize.height)})
-      , frame4 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 4, 0, frameSize.width, frameSize.height)})
-      , frame5 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 5, 0, frameSize.width, frameSize.height)})
-      , frame6 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 6, 0, frameSize.width, frameSize.height)})
-      , frame7 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 7, 0, frameSize.width, frameSize.height)})
-      , frame8 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 8, 0, frameSize.width, frameSize.height)})
-      , frame9 = new cocos.SpriteFrame({texture: texture, rect: geo.rectMake(frameSize.width * 3 + frameSize.width * 9, 0, frameSize.width, frameSize.height)})
-
-    var sprite = new cocos.nodes.Sprite({frame: frame0});
-    this.addChild(sprite);
-
-    var animFrames = [
-        frame0,
-        frame1,
-        frame2,
-        frame3,
-        frame4,
-        frame5,
-        frame6,
-        frame7,
-        frame8,
-        frame9
-    ];
-
-    var animation = new cocos.Animation({frames: animFrames, delay: 0.1})
-      , animate   = new cocos.actions.Animate({animation: animation, restoreOriginalFrame: false})
-      , seq       = new cocos.actions.Sequence({
-            actions: [
-                animate
-            ]
-        })
-
-    sprite.runAction(new cocos.actions.RepeatForever(seq));
-
-    sprite.anchorPoint = new geo.Point(0, 0)
-    // end animation
+    this.texture = new cocos.Texture2D({ file: "/resources/player.png" })
+    this.sprite = new cocos.nodes.Sprite();
+    this.addChild({ child:this.sprite});
+    this.sprite.anchorPoint = new geo.Point(0, 0)
+    this.setAnimation('standingDown')
 
     this.anchorPoint = new geo.Point(0.5, 0.5);
-    this.speed = 180;
+    this.speed = 70;
     this.movement = { 
         up    : false,
         down  : false,
@@ -108,10 +68,80 @@ Player.inherit(cocos.nodes.Node, {
             }
         }
 
+        if (vel.y == 0 && vel.x == 0) animationName = 'stop'
+        if (vel.y < 0) animationName = 'walkingDown'
+        if (vel.y > 0) animationName = 'walkingUp'
+        if (vel.x > 0) animationName = 'walkingRight'
+        if (vel.x < 0) animationName = 'walkingLeft'
+
+        this.setAnimation(animationName)
+
         this.position.x += vel.x
         this.position.y += vel.y
     }
 });
+
+Player.prototype.setAnimation = function setAnimation(animationName) {
+    if (animationName == 'stop') {
+        switch (this.animationName) {
+            case 'walkingUp':
+                animationName = 'standingUp';
+                break;
+            case 'walkingLeft':
+                animationName = 'standingLeft';
+                break;
+            case 'walkingRight':
+                animationName = 'standingRight';
+                break;
+            default:
+                animationName = 'standingDown';
+                break;
+        }
+    }
+
+    if (this.animationName == animationName) return false
+
+    var animationData = this.animation[animationName]
+    var animFrames = [];
+
+    this.animationName = animationName
+
+    for (var i in animationData.frames) {
+        animFrames.push(new cocos.SpriteFrame({
+            texture: this.texture, 
+            rect: geo.rectMake(
+                this.frameSize.width * animationData.frames[i][0], 
+                this.frameSize.height * animationData.frames[i][1], 
+                this.frameSize.width, 
+                this.frameSize.height
+            )
+        }))
+    }
+
+    var animation = new cocos.Animation({frames: animFrames, delay: 0.075})
+      , animate   = new cocos.actions.Animate({animation: animation, restoreOriginalFrame: false})
+      , seq       = new cocos.actions.Sequence({
+            actions: [
+                new cocos.actions.FlipX({ flipX: animationData.mirror }),
+                animate
+            ]
+        })
+    
+    this.sprite.runAction(new cocos.actions.RepeatForever(seq));
+}
+
+Player.prototype.frameSize = new geo.Size(30, 29)
+
+Player.prototype.animation = {
+    'standingDown'    : {frames:[[0,0]], mirror:false},
+    'walkingDown'     : {frames:[[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0]], mirror:false},
+    'standingUp'    : {frames:[[0,1]], mirror:false},
+    'walkingUp'     : {frames:[[1,1],[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1],[9,1],[10,1]], mirror:false},
+    'standingLeft'    : {frames:[[0,1]], mirror:false},
+    'walkingLeft'     : {frames:[[1,2],[2,2],[3,2],[4,2],[5,2],[6,2],[7,2],[8,2],[9,2],[10,2]], mirror:false},
+    'standingRight'    : {frames:[[0,1]], mirror:true},
+    'walkingRight'     : {frames:[[1,2],[2,2],[3,2],[4,2],[5,2],[6,2],[7,2],[8,2],[9,2],[10,2]], mirror:true},
+}
 
 module.exports = Player;
 
