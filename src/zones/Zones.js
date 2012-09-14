1dd1ab31ce15d5e3fade63fdf72dbe0440502c83
Steve Phillips <kingcoyote85@gmail.com>
2012-09-14 08:51:48 -0700

    replaced the generic house with the rough prototype of a store

--- src/zones/Zones.js
+++ src/zones/Zones.js
@@ -43,47 +43,38 @@ module.exports = {
         },
         startPoint : {
             'default': { x: 200, y: 100 },
-            'house' : { x:290,y:270 }
+            'store' : { x:290,y:270 }
         },
         doors : {
-            house : { x:278,y:326,width:45,height:50,zone:'house',name:'entrance' }
+            store : { x:278,y:326,width:45,height:50,zone:'store',name:'entrance' }
         }
     },
-    house : {
+    store : {
         items : [
             {
                 type: 'sprite',
-                position: { x:0, y:0 },
-                size: { width:640, height:480 },
-                image:  '/resources/houseInside.png' ,
+                position: { x:-30, y:-10 },
+                size: { width:700, height:500 },
+                image:  '/resources/storeConcept.png' ,
                 sandbag: [
-                    { x:110,y:0,width:10,height:480 },
-                    { x:520,y:0,width:10,height:480 },
-                    { x:0,y:360,width:640,height:10 },
-                    { x:0,y:80,width:295,height:40 },
-                    { x:330,y:80,width:315,height:40 },
-                    { x:0,y:70,width:640,height:20 }
+                    { x:0,y:163,width:700,height:337 },
+                    { x:480,y:0,width:217,height:162 },
+                    { x:162,y:143,width:56,height:20 },
+                    { x:0,y:0,width:160,height:140 },
+                    { x:-10,y:0,width:700,height:10 }
                 ],
                 z: -1
             },
-            {
-                type : 'sprite',
-                position: { x:200, y:200 },
-                size: { width:16, height:16 },
-                image:  '/resources/box.png' ,
-                sandbag: true,
-                z: 1
-            }
         ],
         position : { 
             x: 0,
             y: 0
         },
         startPoint : {
-            'entrance' : { x: 300, y: 130 }
+            'entrance' : { x: 308, y: 15 }
         },
         doors : {
-            entrance : { x:295, y:90, width:30, height:10, zone:'main', name:'house' }
+            entrance : { x:308, y:0, width:68, height:10, zone:'main', name:'store' }
         },
         scrollable : false
     }
