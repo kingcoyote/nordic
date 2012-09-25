99816b6f1a1b845959e1f01fa78f26a05c372733
Steve Phillips <kingcoyote85@gmail.com>
2012-09-24 21:51:17 -0700

    added a basic main menu

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -8,13 +8,42 @@ var MainMenu = {
         // Create a scene and layer
         var scene = new cocos.nodes.Scene()
 
-        setTimeout(function() {
-            cocos.Director.sharedDirector.pushScene(Scene.create('Game'))
-        }, 2000)
+        scene.addChild(new MenuLayer())
 
         return scene;
     }
 }
+
+var MenuLayer = function() {
+    MenuLayer.superclass.constructor.call(this)
+
+    var startMenu = new cocos.nodes.MenuItem({ callback: function(){ 
+        cocos.Director.sharedDirector.pushScene(Scene.create('Game')); 
+    }})
+    var label = new cocos.nodes.Label({string: 'Start'});
+    startMenu.contentSize = label.contentSize
+    startMenu.addChild(label)
+
+    this.menuItems = [
+        startMenu
+    ]
+
+    var menu = new cocos.nodes.Menu({
+        items: this.menuItems
+    })
+
+    menu.mouseDown = function(e) {
+        this.children[e.button].activate()
+    }
+
+    menu.anchorPoint = new geo.Point(0,0)
+
+    this.addChild(menu)
+
+    this.isMouseEnabled = true
+}
+
+MenuLayer.inherit(cocos.nodes.Layer)
 
 module.exports = MainMenu;
 
