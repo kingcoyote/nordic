d53a9102fc41c764f511182971dffb0cbb696526
Steve Phillips <kingcoyote85@gmail.com>
2012-09-25 07:24:18 -0700

    fixed up the menu items so they can be clicked properly

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -21,22 +21,44 @@ var MenuLayer = function() {
         cocos.Director.sharedDirector.pushScene(Scene.create('Game')); 
     }})
     var label = new cocos.nodes.Label({string: 'Start'});
+    label.anchorPoint = new geo.Point(0, 0)
     startMenu.contentSize = label.contentSize
     startMenu.addChild(label)
+    startMenu.label = label
+    startMenu.position = new geo.Point(320, 240)
+
+    var aboutMenu = new cocos.nodes.MenuItem({ callback: function(){ 
+        alert("It's a game!")
+    }})
+    var label = new cocos.nodes.Label({string: 'About'});
+    label.anchorPoint = new geo.Point(0, 0)
+    aboutMenu.position = new geo.Point(startMenu.contentSize.width + 15, 0)
+    aboutMenu.contentSize = label.contentSize
+    aboutMenu.addChild(label)
+    aboutMenu.label = label
+    aboutMenu.position = new geo.Point(320, 200)
 
     this.menuItems = [
-        startMenu
+        startMenu,
+        aboutMenu
     ]
 
     var menu = new cocos.nodes.Menu({
         items: this.menuItems
     })
 
+    var self = this
+
     menu.mouseDown = function(e) {
-        this.children[e.button].activate()
+        for(var i in self.menuItems) {
+            if (geo.rectContainsPoint(self.menuItems[i].boundingBox, e.locationInCanvas)) {
+                self.menuItems[i].activate()
+            }
+        }
     }
 
     menu.anchorPoint = new geo.Point(0,0)
+    menu.position = new geo.Point(0, 0)
 
     this.addChild(menu)
 
